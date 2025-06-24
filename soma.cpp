91d0144c32acbf355d9f8e0fe5778c6091be53a6
Felipe Phelps <gphfox@gmail.com>
2025-06-24 20:46:15 +0100

    base project

---
+++ soma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int res;
+
+    cout << "first number: "<<endl;
+    cin >> a;
+    cout << "second number: "<< endl;
+    cin >> b;
+
+    res = a+b;
+
+    return 0;
+
+
+}
